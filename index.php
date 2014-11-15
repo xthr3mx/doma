750f01a4896ce97e52f93e5a72797ca32d1bf296
xthr3mx <xthr3mx@gmail.com>
2014-11-14 22:12:33 -0600

    Insertar y visualizar datos ingresados (PHP).

--- index.php
+++ index.php
@@ -4,6 +4,71 @@
 </head>
 <body>
 
-  <p>Bienvenido!, estamos utilizando <?php echo phpversion(); ?></p>
+	<form method="post" action="index.php">
+		<label for="nombre">Nombe</label>
+		<input type="text" name="nombre" id="nombre" />
+		<br />
+		<label for="email">Email</label>
+		<input type="text" name="email" id="email" />
+		<br />
+		<input type="submit" name="submit" value="Registrar" />
+	</form>
+
+<?php 
+	$host = "us-cdbr-azure-northcentral-a.cleardb.com";
+	$username = "b8e21eb547cfc6";
+	$password = "d4494e32";
+	$database_name = "demodomsav1";
+
+	try{
+		$connection = new PDO("mysql:host=$host;dbname=$database_name",$username,$password);
+		$connection->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(Exception $exception){
+		die(var_dump($exception)),
+	}
+	if(!empty($_POST))
+	{
+		try{
+			$nombre = $_POST['nombre'];
+			$email = $_POST['email'];
+			$fecha = date("Y-m-d");
+			$sql_insert = "INSERT INTO registros (nombre,email,fecha) VALUES (?,?,?)";
+			$stmt=$connection->prepare($sql_insert);
+			$stmt->bindValue(1,$nombre);
+			$stmt->bindValue(2,$email);
+			$stmt->bindValue(3,$fecha);
+			$stmt->execute();
+			echo "<h3>Se ha registrado.</h3>";
+		}catch(Exception $exception){
+			echo "<h3>No se pudo registrar.</h3>";
+			die(var_dump($exception));
+		}
+	}
+
+	$sql_select = "SELECT * FROM registros";
+	$stmt = $connection->query($sql_select);
+	$registrosSql = $stmt->fetchAll();
+	if(count($registrosSql)>0){
+		echo 	"<h2>Personas registradas:</h2>".
+				"<table>".
+				"<tr>"
+				"<td>Nombre</td>".
+				"<td>Email</td>".
+				"<td>Fecha</td>".
+				"</tr>";
+		foreach($registro as $registrosSql){
+			echo 	"<tr>" .
+					"<td>".$registro['nombre']."</td>".
+					"<td>".$registro['email']."</td>".
+					"<td>".$registro['fecha']."</td>".
+					"</tr>";
+		}
+		echo 	"</table>";
+	}
+	else{ echo "<h3>No existen registros.</h3>"; }
+	$connection = null;
+
+?>
+
 </body>
 </html>
